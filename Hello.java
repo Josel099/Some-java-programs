df66ccb5441fc00eaf3fa05828f0249d8b17e46d
Josel099 <joseltech2002@gmail.com>
2023-02-03 15:54:26 +0530

    java program  file2 added

---
+++ Hello.java
@@ -0,0 +1,26 @@
+package oops;
+
+ class Hello{
+    public static void main(String ar[]){
+
+        sample s1=new sample();
+        sample s2=new sample();
+        s1.a=10;
+        s1.b=3;
+        s1.c=4;
+        s2.a=45;
+        s2.b=41;
+     
+        System.out.println("s1-a: "+s1.a+" s1.c: "+ s1.c+" s1.b: "+s1.b+" s2-b: "+s2.b +" s2.a: "+ s2.a);
+        s1.display();
+        s2.display();
+
+       
+ 
+
+
+
+
+
+    }
+}

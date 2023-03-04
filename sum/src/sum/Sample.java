e91e01918d5af3427c8968418882d7528e9f3218
Josel099 <joseltech2002@gmail.com>
2023-03-04 23:54:40 +0530

    Sum is added

---
+++ sum/src/sum/Sample.java
@@ -0,0 +1,13 @@
+package sum;
+
+public class Sample {
+int a,b,c;
+void CalculateSum() {
+	c=a+b;
+}
+void display() {
+	CalculateSum();
+	System.out.println(c);
+	
+}
+}
---
+++ sum/src/sum/SumDisplay.java
@@ -0,0 +1,18 @@
+package sum;
+
+public class SumDisplay {
+	public static void main(String ar[])
+	{
+		Sample s1=new Sample();
+		Sample s2=new Sample();
+		s1.a=12;
+		s2.a=2;
+		s1.b=5;
+		s2.b=6;
+		//s1.CalculateSum();
+		s1.display();
+		s2.display();
+		
+	}
+
+}

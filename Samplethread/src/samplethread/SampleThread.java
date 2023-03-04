3e9c377864f2ae81702ba867eaa7af787ee03c8a
Josel099 <joseltech2002@gmail.com>
2023-03-04 23:52:40 +0530

    Samplethread is added

---
+++ Samplethread/src/samplethread/SampleThread.java
@@ -0,0 +1,23 @@
+package samplethread; 
+public class SampleThread extends Thread{
+
+	
+		int a;
+		SampleThread(int a){
+			this.a=a;
+		}
+		public void run() {
+			
+			for(int i=0;i<10;i++)
+			{
+				System.out.println(a+"thread count "+i);
+				try {
+				Thread.sleep(1000);
+				}
+				catch(InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	
+}

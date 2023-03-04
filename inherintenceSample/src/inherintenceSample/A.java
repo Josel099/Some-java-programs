78c69e5b37215f074e8eb55b4c164674292ec90e
Josel099 <joseltech2002@gmail.com>
2023-03-04 23:49:35 +0530

    inheritence   is added

---
+++ inherintenceSample/src/inherintenceSample/A.java
@@ -0,0 +1,12 @@
+package inherintenceSample;
+
+public class A {
+
+	void diplay() {
+		System.out.println("welcome to class A");
+	}
+	A(){
+		System.out.println("parent class a worked ");
+	}
+	
+}
---
+++ inherintenceSample/src/inherintenceSample/Sample.java
@@ -0,0 +1,11 @@
+package inherintenceSample;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		B b=new B();
+		//b.diplay();
+		//b.diplayB();
+	}
+	
+}

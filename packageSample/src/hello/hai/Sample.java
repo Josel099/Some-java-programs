5784f14f418aa33e5e17c6c06cd57757595a8301
Josel099 <joseltech2002@gmail.com>
2023-03-04 23:50:51 +0530

    packageSample is added

---
+++ packageSample/src/hello/hai/Sample.java
@@ -0,0 +1,7 @@
+package hello.hai;
+import hello.hey.Hello;				// importing hello.hey package and its class hello // * can be used to acess to all classes in the package
+public class Sample {
+	
+	Hello h=new Hello(); 
+
+}

ef0ad185ff305c7d072a22fb29fd011d2989f15a
Josel099 <joseltech2002@gmail.com>
2023-03-04 23:49:35 +0530

    inheritence   is added

---
+++ inherintenceSample/src/inherintenceSample/B.java
@@ -0,0 +1,13 @@
+package inherintenceSample;
+
+public class B extends A{
+	
+	void diplayB() {
+	System.out.println("welcome to class B");
+	}
+	B(){
+		System.out.println("parent class B worked ");  
+	}
+	
+
+}

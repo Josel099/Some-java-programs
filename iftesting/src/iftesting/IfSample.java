514869857df6d82055c463f2874f7ddbe82694e9
Josel099 <joseltech2002@gmail.com>
2023-03-04 23:49:10 +0530

    ifTesting   is added

---
+++ iftesting/src/iftesting/IfSample.java
@@ -0,0 +1,24 @@
+package iftesting;
+
+
+	import java.util.Scanner;
+	public class IfSample {
+	public static void main(String ar[]) {
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("enter a number");
+		
+		int a =sc.nextInt();
+		if(a<0) {
+			System.out.println("negative number");
+		}
+		else if(a>0) {
+			System.out.println("positive number");
+		}
+		else 
+			System.out.println("zero");
+	}
+	}
+
+	
+
